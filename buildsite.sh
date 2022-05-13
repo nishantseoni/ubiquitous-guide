26f1443c86aaa8465b389f2d9b74a35139158416
Nishant <nishant@fucku.com>
2022-05-13 12:04:32 -0700

    deleted unnecessary file

---
+++ buildsite.sh
@@ -0,0 +1,7 @@
+mkdir {templates, docs, content, images, css}
+git add -A
+git commit -m 'making directories'
+mv first-style-sheet.css css/
+git commit -m 'moving css'
+mv contact.html about.html portfolio.html content/
+
